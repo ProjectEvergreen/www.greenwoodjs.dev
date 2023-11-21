fe6f770dfe96b5316881e58be6f227446044c0a6
Owen Buckley <owenbuckley13@gmail.com>
2023-11-21 16:27:32 -0500

    very naive implementation

--- greenwood.config.js
+++ greenwood.config.js
@@ -1,6 +1,7 @@
 import { greenwoodPluginImportRaw } from "@greenwood/plugin-import-raw";
+import { greenwoodPluginCssModules } from "./plugin-css-modules.js";
 
 export default {
   prerender: true,
-  plugins: [greenwoodPluginImportRaw()],
+  plugins: [greenwoodPluginImportRaw(), greenwoodPluginCssModules()],
 };
