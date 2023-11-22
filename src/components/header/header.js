d5e8cf787146b04e6c2cee5dd80e082caa18b06a
Owen Buckley <owenbuckley13@gmail.com>
2023-11-22 14:02:05 -0500

    support recursively walking module imports looking for CSS modules

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,10 +1,13 @@
+import '../navigation/navigation.js';
 import styles from './header.module.css';
 
 export default class Header extends HTMLElement {
   connectedCallback() {
-    console.log({ styles });
     this.innerHTML = `
-      <h1 class="${styles.heading}">Welcome to Greenwood!</h1>
+      <header>
+        <h1 class="${styles.heading}">Welcome to Greenwood!</h1>
+        <app-navigation></app-navigation>
+      </header>
     `;
   }
 }
