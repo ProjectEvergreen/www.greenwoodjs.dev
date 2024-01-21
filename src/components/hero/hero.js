ce63f4966a5aa176d6e25654154ff05d5749a2c5
Owen Buckley <owenbuckley13@gmail.com>
2024-01-21 18:33:52 -0500

    Constructable stylesheets

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -1,13 +1,20 @@
-// import html from './hero.html?type=html';
+// CSS Modules would also be really nice here!
+// might work better here for pure static content instead of needing a <style> tag
+// npte: we don't need a plugin for this in the browser (but likely will need to adapt this for NodeJS / bundling)
+import css from './hero.css' with { type: "css" };
 import html from "./hero.html" with { type: "html" };
 
-console.log({ html });
-// CSS Modules would also be really nice here!
 export default class HeroBanner extends HTMLElement {
   connectedCallback() {
-    this.innerHTML = `
-      ${html}
-    `
+    if(!this.shadowRoot) {
+      console.log({ css, html });
+      const template = document.createElement('template');
+      template.innerHTML = html;
+
+      this.attachShadow({ mode: 'open' });
+      this.shadowRoot.appendChild(template.content.cloneNode(true));
+      this.shadowRoot.adoptedStyleSheets = [css];
+    }
   }
 }
 
