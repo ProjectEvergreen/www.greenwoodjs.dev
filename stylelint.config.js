4424217bd72260647b2aeafeadcc6108b9166aaa
Owen Buckley <owenbuckley13@gmail.com>
2024-04-29 21:24:58 -0400

    document linting and formatting

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,4 +1,6 @@
 export default {
+  // no stylelint 16 support yet
+  // https://github.com/developer-stylechain/stylelint-a11y/issues/4
   // plugins: ['stylelint-a11y'],
   extends: ['stylelint-prettier/recommended'],
 };
