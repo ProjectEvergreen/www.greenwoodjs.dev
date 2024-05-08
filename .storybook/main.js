7bb8bc5009e6b8cc043111727b34ba1ab232317d
Owen Buckley <owenbuckley13@gmail.com>
2024-05-07 21:46:56 -0400

    document optional dep

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,6 @@
 /** @type { import('@storybook/web-components-vite').StorybookConfig } */
+// had to add @rollup/rollup-linux-x64-gnu as an optional dep
+// https://github.com/vitejs/vite/discussions/15532
 const config = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@chromatic-com/storybook"],
