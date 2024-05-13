6c9504a38756ed4fc487a9173a2a2a9282d74029
Owen Buckley <owenbuckley13@gmail.com>
2024-05-13 16:24:13 -0400

    refactor hardcoded conditions

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from "vite";
 import fs from "fs";
 import path from "path";
+import { greenwoodPluginStandardCss } from '@greenwood/cli/src/plugins/resource/plugin-standard-css.js';
+
+const standardCssResource = greenwoodPluginStandardCss.provider({});
 
 // Vite doesn't support import attributes :/
 // https://github.com/vitejs/vite/pull/15654
@@ -9,20 +12,22 @@ import path from "path";
 // https://github.com/vitejs/vite/issues/12140
 // for now, bail out of any standard CSS handling
 // https://github.com/vitejs/vite/issues/15322#issuecomment-1858878697
-// TODO dont hardcode load condition
 function transformConstructableStylesheetsPlugin() {
   return {
     name: "transform-css-module-scripts",
     enforce: "pre",
-    resolveId: (id, importer) =>
-      // add .type so CSS modules are not precessed by the default pipeline
-      id.endsWith("header.css") ? path.join(path.dirname(importer), `${id}.type`) : undefined,
-    load: (id) => {
-      if (id.endsWith(".css.type")) {
+    resolveId: (id, importer) => {
+      if (importer.indexOf('/src/components/') >= 0 && id.endsWith('.css')) {
+        // add .type so CSS modules are not precessed by the default pipeline
+        return path.join(path.dirname(importer), `${id}.type`);
+      }
+    },
+    load: async (id) => {
+      if (id.indexOf('/src/components/') >= 0 && id.endsWith(".css.type")) {
         const filename = id.slice(0, -5);
-        const content = fs.readFileSync(filename, "utf-8");
-        const contents = content.replace(/\r?\n|\r/g, " ").replace(/\\/g, "\\\\");
-        const body = `const sheet = new CSSStyleSheet();sheet.replaceSync(\`${contents}\`);export default sheet;`;
+        const contents = fs.readFileSync(filename, "utf-8");
+        const response = await standardCssResource.intercept(null, null, new Response(contents))
+        const body = await response.text();
 
         return body;
       }
