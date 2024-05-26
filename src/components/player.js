8919c3170aee9e302ca6ede63b67614e5e9e10af
Owen Buckley <owenbuckley13@gmail.com>
2024-05-25 20:36:20 -0400

    refactor example

--- src/components/player.js
+++ src/components/player.js
@@ -1,21 +1,44 @@
 export default class Player extends HTMLElement {
-  async connectedCallback() {
-    console.log("conntected!");
-    const playlist = JSON.parse(this.getAttribute("playlist") || "[]");
-    const title = this.getAttribute("title") || "";
-    const list = playlist
-      .map((item) => {
-        return `<li>${item.title}</li>`;
-      })
-      .join("\n");
-    console.log({ playlist, title, list });
+  constructor() {
+    super();
 
-    this.innerHTML = `
-      <h1>${title}</h1>
-      <ul style="text-align: left; width: 30%; margin:0 auto;">
-        ${list}
-      </ul>
-    `;
+    this.title = "";
+    this.playlist = [];
+  }
+
+  async connectedCallback() {
+    this.playlist = JSON.parse(this.getAttribute("playlist") || "[]");
+    this.title = this.getAttribute("name") || "";
+
+    if (!this.shadowRoot) {
+      this.playlist = JSON.parse(this.getAttribute("playlist") || "[]");
+      this.title = this.getAttribute("name") || "";
+      const list = this.playlist.map((item) => `<li>${item.title}</li>`).join("\n");
+      const template = document.createElement("template");
+
+      template.innerHTML = `
+        <div style="text-align: left; width: 30%; margin:0 auto;">
+          <h1>${this.title}</h1>
+          <ul>
+            ${list}
+          </ul>
+          <button>Start Playlist</button>
+        </div>
+      `;
+
+      this.attachShadow({ mode: "open" });
+      this.shadowRoot.appendChild(template.content.cloneNode(true));
+    }
+
+    this.shadowRoot
+      .querySelector("button")
+      ?.addEventListener("click", this.startPlaylist.bind(this));
+  }
+
+  startPlaylist() {
+    const { title, playlist } = this;
+    console.log("starting playlist =>", { title, playlist });
+    alert(`starting playlist => ${title}`);
   }
 }
 
