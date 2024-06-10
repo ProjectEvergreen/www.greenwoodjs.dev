b508f5b6a24deff474430962a2993ad0b632b986
Owen Buckley <owenbuckley13@gmail.com>
2024-06-10 16:07:44 -0400

    css modules implementation refactoring

--- greenwood.config.js
+++ greenwood.config.js
@@ -1,9 +1,10 @@
 import { greenwoodPluginImportRaw } from "@greenwood/plugin-import-raw";
 import { greenwoodPluginCssModules } from "./plugin-css-modules.js";
+import { greenwoodPluginPostCss } from "@greenwood/plugin-postcss";
 
 export default {
   prerender: true,
-  plugins: [greenwoodPluginImportRaw(), greenwoodPluginCssModules()],
+  plugins: [greenwoodPluginPostCss(), greenwoodPluginCssModules(), greenwoodPluginImportRaw()],
   markdown: {
     plugins: ["@mapbox/rehype-prism"],
   },
--- src/components/walkthrough/walkthrough.js
+++ src/components/walkthrough/walkthrough.js
@@ -1,47 +1,55 @@
-// import sheet from './walkthrough.css' with { type: 'css' };
-// import theme from '../../styles/theme.css' with { type: 'css' };
+import sheet from "./walkthrough.css" with { type: "css" };
+import theme from "../../styles/theme.css" with { type: "css" };
 
-const template = document.createElement('template');
+const template = document.createElement("template");
 
 export default class Walkthrough extends HTMLElement {
   constructor() {
     super();
     this.index = 0;
     this.cards = [];
-  };
+  }
 
   connectedCallback() {
-    if (!this.shadowRoot && typeof window !== 'undefined') {
-      this.cards = globalThis.document?.querySelectorAll('.walkthrough-card') || [];
+    if (!this.shadowRoot) {
+      this.cards = globalThis.document?.querySelectorAll(".walkthrough-card") || [];
 
       template.innerHTML = `
-        <h3>${this.cards[this.index].querySelector('p').innerHTML}</h3>
+        <div class="walkthrough">
+          <h3>${this.cards[this.index]?.querySelector("p").innerHTML}</h3>
 
-        <ol>
-          ${Array.from(this.cards).map((card, idx) => {      
-            const label = card.querySelector('span').innerHTML;
+          <ol>
+            ${Array.from(this.cards)
+              .map((card, idx) => {
+                const label = card?.querySelector("span").innerHTML;
 
-            return `<li data-idx="${idx}">${label}</li>`;
-          }).join('')}
-        </ol>
+                return `<li data-idx="${idx}">${label}</li>`;
+              })
+              .join("")}
+          </ol>
 
-        <div class="snippet">${this.cards[this.index].querySelector('pre').innerHTML}</div>
+          <div class="snippet">${this.cards[this.index]?.querySelector("pre").innerHTML}</div>
+        </div>
       `;
 
       this.attachShadow({ mode: "open" });
       this.shadowRoot.appendChild(template.content.cloneNode(true));
     }
 
-    // this.shadowRoot.adoptedStyleSheets = [ theme ];
-    this.shadowRoot?.querySelectorAll('li')?.forEach(item => item.addEventListener('click', this.selectItem.bind(this)))
+    this.shadowRoot.adoptedStyleSheets = [theme, sheet];
+    this.shadowRoot
+      .querySelectorAll("li")
+      ?.forEach((item) => item.addEventListener("click", this.selectItem.bind(this)));
   }
 
   selectItem(event) {
-    console.log('selectItem', event.currentTarget.dataset.idx);
+    console.log("selectItem", event.currentTarget.dataset.idx);
     this.index = event.currentTarget.dataset.idx;
-    this.shadowRoot.querySelector('h3').innerHTML = this.cards[this.index].querySelector('p').innerHTML;
-    this.shadowRoot.querySelector('.snippet').innerHTML = this.cards[this.index].querySelector('pre').innerHTML;
+    this.shadowRoot.querySelector("h3").innerHTML =
+      this.cards[this.index].querySelector("p").innerHTML;
+    this.shadowRoot.querySelector(".snippet").innerHTML =
+      this.cards[this.index].querySelector("pre").innerHTML;
   }
 }
 
-customElements.define('app-walkthrough', Walkthrough);
+customElements.define("app-walkthrough", Walkthrough);
