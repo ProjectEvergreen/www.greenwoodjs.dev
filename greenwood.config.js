ada388978ae3e55c4d9bc9eacf64936d95de74b0
Owen Buckley <owenbuckley13@gmail.com>
2024-06-10 17:26:59 -0400

    syntax highlighting

--- greenwood.config.js
+++ greenwood.config.js
@@ -4,4 +4,7 @@ import { greenwoodPluginCssModules } from "./plugin-css-modules.js";
 export default {
   prerender: true,
   plugins: [greenwoodPluginImportRaw(), greenwoodPluginCssModules()],
+  markdown: {
+    plugins: ["@mapbox/rehype-prism"],
+  },
 };
---
+++ src/pages/index.md
@@ -0,0 +1,14 @@
+---
+imports:
+  - ../styles/home.css
+---
+
+# Welcome to Greenwood!
+
+```js
+return new Response('<h1>Hello World</h1>', {
+  headers: {
+    'Content-Type': 'text/html'
+  }
+})
+```
