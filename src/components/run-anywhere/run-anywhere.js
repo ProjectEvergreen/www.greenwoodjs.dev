c38ea61504c0c5c30c492b4f081b0515298637d7
Owen Buckley <owenbuckley13@gmail.com>
2024-06-24 21:47:32 -0400

    run anywhere platform icon boxes

--- src/components/run-anywhere/run-anywhere.js
+++ src/components/run-anywhere/run-anywhere.js
@@ -1,13 +1,33 @@
+import platforms from "./platforms.json" with { type: "json" };
 import styles from "./run-anywhere.module.css";
 
 export default class RunAnywhere extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
       <div class="${styles.container}">
-        <h3>Run anywhere the run can run</h3>
+        <h3 class="${styles.heading}">Run anywhere the web can run</h3>
+        
         <p>Greenwood Lorum Ipsum...</p>
-        <img src="/assets/vercel.svg" alt="Vercel logo"/>
-        <img src="/assets/netlify.png" alt="Netlify logo"/>
+
+        <div class="${styles.iconContainer}">
+          <div class="${styles.platformColumn}">
+            ${platforms
+              .map((platform) => {
+                const { name, icon, link } = platform;
+
+                return `
+                <div class="${styles.platformBox}">
+                  <div class="${styles.iconBox}">
+                    <img class="${styles.icon}" src="${icon}" alt="${name} logo"/>
+                  </div>
+
+                  <a class="${styles.iconLink}" href="${link}">${name}</a>
+                </div>
+              `;
+              })
+              .join("")}
+          </div>
+        </div>
       </div>
     `;
   }
