6d4290c6b9824b425faa27fa0690b0ff5b42be8f
Owen Buckley <owenbuckley13@gmail.com>
2024-06-30 15:22:36 -0400

    styling and content for build with friends section

--- src/components/build-with-friends/build-with-friends.js
+++ src/components/build-with-friends/build-with-friends.js
@@ -1,12 +1,36 @@
 import styles from "./build-with-friends.module.css";
+import htmxIcon from "../../assets/htmx.svg?type=raw";
+import litIcon from "../../assets/lit.svg?type=raw";
+import storybookIcon from "../../assets/storybook.svg?type=raw";
+import tailwindIcon from "../../assets/tailwind-logo.svg?type=raw";
+import typescriptIcon from "../../assets/typescript.svg?type=raw";
+import wtrIcon from "../../assets/modern-web.svg?type=raw";
 
 export default class BuildWithFriends extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
       <div class="${styles.container}">
-        <h3>Build With Friends</h3>
-        <p>Greenwood Lorum Ipsum...</p>
-        <img src="/assets/lit.svg" alt="Lit logo"/>
+        <h3 class="${styles.heading}">Build With Friends</h3>
+        <p>Greenwood and Web Components work great with all your favorite tools in the frontend ecosystem.</p>
+        <span class="${styles.icon}">
+          ${litIcon}
+        </span>
+        <span class="${styles.icon}">
+          ${storybookIcon}
+        </span>
+        <span class="${styles.icon}">
+          ${tailwindIcon}
+        </span>
+        <span class="${styles.icon}">
+          ${typescriptIcon}
+        </span>
+        <span class="${styles.icon}">
+          ${htmxIcon}
+        </span>
+        <span class="${styles.icon}">
+          <span>${wtrIcon}</span>
+          <span>Web Test Runner</span>
+        </span>
       </div>
     `;
   }
