53a5fb8e0c2356ff722fe92f18efdd4554441e2d
Owen Buckley <owenbuckley13@gmail.com>
2024-07-01 12:27:03 -0400

    why greenwood skeleton

--- src/components/why-greenwood/why-greenwood.js
+++ src/components/why-greenwood/why-greenwood.js
@@ -1,11 +1,32 @@
 import styles from "./why-greenwood.module.css";
+import placeholderIcon from "../../assets/api-routes.svg?type=raw";
 
 export default class WhyGreenwood extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
       <div class="${styles.container}">
-        <h3>Why Greenwood?</h3>
-        <p>Greenwood Lorum Ipsum...</p>
+        <h3 class="${styles.heading}">Why Greenwood?</h3>
+        <p class="${styles.subHeading}">Greenwood lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+
+        <div class="${styles.cardContainer}">
+          <div class="${styles.card}">
+            <h3>${placeholderIcon} Something 1</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <a href="#">Link</a>
+          </div>
+
+          <div class="${styles.card}">
+            <h3>${placeholderIcon} Something 2</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <a href="#">Link</a>
+          </div>
+
+          <div class="${styles.card}">
+            <h3>${placeholderIcon} Something 3</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <a href="#">Link</a>
+          </div>
+        </div>
       </div>
     `;
   }
