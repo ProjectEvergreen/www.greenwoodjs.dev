30ff2ca0488fec5e70a51bf2456bc4004bc11aea
Owen Buckley <owenbuckley13@gmail.com>
2024-07-11 14:29:35 -0400

    refactor header test case

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,64 +3,66 @@ import githubIcon from "../../assets/github.svg?type=raw";
 import twitterIcon from "../../assets/twitter-logo.svg?type=raw";
 import mobileMenuIcon from "../../assets/tile.svg?type=raw";
 import greenwoodLogo from "../../assets/greenwood-logo-full.svg?type=raw";
-import styles from "./header.module.css" with { type: "css" };
+import styles from "./header.module.css";
 
 export default class Header extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
       <header class="${styles.container}">
         <a href="/" title="Greenwood Home Page" class="${styles.logoLink}">
-          <div>
-            ${greenwoodLogo}
-          </div>
+          ${greenwoodLogo}
         </a>
 
-        <nav class="${styles.navBar}">
-          <ul class="${styles.navBarMenu}">
-            <li class="${styles.navBarMenuItem}">
-              <a href="/docs/" title="Documentation">Docs</a>
-            </li>
-            <li class="${styles.navBarMenuItem}">
-              <a href="/guides/" title="Guides">Guides</a>
-            </li>
-            <li class="${styles.navBarMenuItem}">
-              <a href="/blog/" title="Blog">Blog</a>
-            </li>
-          </ul>
+        <div class="${styles.navBar}">
+          <nav role="navigation" aria-label="Main">
+            <ul class="${styles.navBarMenu}">
+              <li class="${styles.navBarMenuItem}">
+                <a href="/docs/" title="Documentation">Docs</a>
+              </li>
+              <li class="${styles.navBarMenuItem}">
+                <a href="/guides/" title="Guides">Guides</a>
+              </li>
+              <li class="${styles.navBarMenuItem}">
+                <a href="/blog/" title="Blog">Blog</a>
+              </li>
+            </ul>
+          </nav>
 
-          <ul class="${styles.socialTray}">
-            <li class="${styles.socialIcon}">
-              <a href="https://github.com/ProjectEvergreen/greenwood" title="GitHub">
-                ${githubIcon}
-              </a>
-            </li>
+          <nav role="navigation" aria-label="Social">
+            <ul class="${styles.socialTray}">
+              <li class="${styles.socialIcon}">
+                <a href="https://github.com/ProjectEvergreen/greenwood" title="GitHub">
+                  ${githubIcon}
+                </a>
+              </li>
 
-            <li class="${styles.socialIcon}">
-              <a href="https://discord.gg/bsy9jvWh" title="Discord">
-                ${discordIcon}
-              </a>
-            </li>
+              <li class="${styles.socialIcon}">
+                <a href="https://discord.gg/bsy9jvWh" title="Discord">
+                  ${discordIcon}
+                </a>
+              </li>
 
-            <li class="${styles.socialIcon}">
-              <a href="https://twitter.com/PrjEvergreen" title="Twitter">
-                ${twitterIcon}
-              </a>
-            </li>
-          </ul>
+              <li class="${styles.socialIcon}">
+                <a href="https://twitter.com/PrjEvergreen" title="Twitter">
+                  ${twitterIcon}
+                </a>
+              </li>
+            </ul>
+          </nav>
 
-          <button class="${styles.mobileMenuIcon}" popovertarget="my-popover">
+          <button class="${styles.mobileMenuIcon}" popovertarget="mobile-menu" aria-label="Mobile Menu Icon Button">
             ${mobileMenuIcon}
           </button>
-        </nav>
+        </div>
 
-        <div id="my-popover" popover="manual">
+        <div id="mobile-menu" popover="manual">
           <div class="${styles.mobileMenuBackdrop}">
 
-            <button class="${styles.mobileMenuCloseButton}" popovertarget="my-popover" popovertargetaction="hide">
+            <button class="${styles.mobileMenuCloseButton}" popovertarget="mobile-menu" popovertargetaction="hide" aria-label="Mobile Menu Close Button">
               &times;
             </button>
             
-            <nav>
+            <nav role="navigation" aria-label="Mobile">
               <ul class="${styles.mobileMenuList}">
                 <li class="${styles.mobileMenuItem}">
                   <a href="/docs/" title="Documentation">Docs</a>
