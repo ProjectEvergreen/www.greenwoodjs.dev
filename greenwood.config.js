b2c6359d9b4640353a8d6c6bf5fad3031017a71d
Owen Buckley <owenbuckley13@gmail.com>
2024-08-27 20:19:15 -0400

    enable active frontmatter

--- greenwood.config.js
+++ greenwood.config.js
@@ -4,7 +4,7 @@ import { greenwoodPluginCssModules } from "./plugin-css-modules.js";
 export default {
   prerender: true,
   plugins: [greenwoodPluginCssModules(), greenwoodPluginImportRaw()],
-  // activeFrontmatter: true,
+  activeFrontmatter: true,
   markdown: {
     plugins: ["@mapbox/rehype-prism", "rehype-slug", "rehype-autolink-headings", "remark-github"],
   },
