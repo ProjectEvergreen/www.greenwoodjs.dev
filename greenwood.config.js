8e31d3a11b8c074ce4c12a149b92129f0496ba9a
Owen Buckley <owenbuckley13@gmail.com>
2024-09-02 14:20:40 -0400

    auto link headings styles

--- greenwood.config.js
+++ greenwood.config.js
@@ -5,6 +5,8 @@ export default {
   prerender: true,
   plugins: [greenwoodPluginCssModules(), greenwoodPluginImportRaw()],
   activeFrontmatter: true,
+  // would be nice if we could customize these plugins, like appending the autolink headings
+  // https://github.com/ProjectEvergreen/greenwood/issues/1247
   markdown: {
     plugins: ["@mapbox/rehype-prism", "rehype-slug", "rehype-autolink-headings", "remark-github"],
   },
