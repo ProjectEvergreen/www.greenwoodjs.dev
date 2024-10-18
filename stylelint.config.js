248262a6d550f5ab172ea7819871d9bae303c92c
Paul Barry <devlab2425@gmail.com>
2024-10-17 23:47:48 -0400

    Replace stylelint plugin

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,6 +1,3 @@
 export default {
-  // no stylelint 16 support yet
-  // https://github.com/developer-stylechain/stylelint-a11y/issues/4
-  // plugins: ['stylelint-a11y'],
-  extends: ["stylelint-prettier/recommended"],
+  extends: ["stylelint-config-recommended"],
 };
