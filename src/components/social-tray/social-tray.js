f80ce93b205e5c5893b9b932377f2df6a82e3640
Owen Buckley <owenbuckley13@gmail.com>
2025-01-24 20:02:34 -0500

    social tray component and header integration

---
+++ src/components/social-tray/social-tray.js
@@ -0,0 +1,35 @@
+import styles from "./social-tray.module.css";
+import discordIcon from "../../assets/discord.svg?type=raw";
+import githubIcon from "../../assets/github.svg?type=raw";
+import twitterIcon from "../../assets/twitter-logo.svg?type=raw";
+
+export default class SocialTray extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+    
+      <nav role="navigation" aria-label="Social">
+        <ul class="${styles.socialTray}">
+          <li class="${styles.socialIcon}">
+            <a href="https://github.com/ProjectEvergreen/greenwood" title="GitHub">
+              ${githubIcon}
+            </a>
+          </li>
+
+          <li class="${styles.socialIcon}">
+            <a href="/discord/" title="Discord">
+              ${discordIcon}
+            </a>
+          </li>
+
+          <li class="${styles.socialIcon}">
+            <a href="https://twitter.com/PrjEvergreen" title="Twitter">
+              ${twitterIcon}
+            </a>
+          </li>
+        </ul>
+      </nav>
+    `;
+  }
+}
+
+customElements.define("app-social-tray", SocialTray);
