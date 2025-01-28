cc1a245e1a519805f4f66f9833ee4dc68a92122a
Owen Buckley <owenbuckley13@gmail.com>
2025-01-27 19:13:57 -0500

    add gfm plugin for table markdown rendering

--- greenwood.config.js
+++ greenwood.config.js
@@ -41,7 +41,13 @@ export default {
   // would be nice if we could customize these plugins, like appending the autolink headings
   // https://github.com/ProjectEvergreen/greenwood/issues/1247
   markdown: {
-    plugins: ["@mapbox/rehype-prism", "rehype-slug", "rehype-autolink-headings", "remark-github"],
+    plugins: [
+      "@mapbox/rehype-prism",
+      "rehype-slug",
+      "rehype-autolink-headings",
+      "remark-github",
+      "remark-gfm",
+    ],
   },
 
   plugins: [
